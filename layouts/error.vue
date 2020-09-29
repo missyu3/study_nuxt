2d7b13945cb4f7cf271cd31bad4ac951330e9092
mitsutaka matsumoto <mitutaka1283@yahoo.co.jp>
2020-09-29 22:46:34 +0900

    Add 404Error

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <h1>Error!</h1>
+    <h1 v-if="error.statusCode === 404">Error! 404</h1>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["error"]
+};
+</script>
